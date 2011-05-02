758f48c3d0950964da02e4f804ab0d2378a6726e
Richard Harrison <richard@rjharrison.org>
2011-05-02 14:24:25 +0100

    Added a config file with an islive flag. This helps us test that we can modify the config on the fly

--- www/index.php
+++ www/index.php
@@ -1,1 +1,9 @@
-<?php echo 'Hello from deployment-test'
+<?php 
+echo "<h1>Hello from deployment-test</h1>";
+
+$conf = parse_ini_file("../conf/live.ini");
+if($conf["islive"]){
+  echo "<p style='color:green'>islive = true</p>";
+}else{
+  echo "<p style='color:red'>islive = false</p>";
+}
